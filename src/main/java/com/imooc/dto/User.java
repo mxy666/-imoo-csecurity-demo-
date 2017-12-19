283efe772d96ffd3cc3d0d225b28da519b6062c9
“maxiaoyu <maxiaoyu@dongsport.com>
2017-12-19 15:17:11 +0800

    JsonView注解使用

--- src/main/java/com/imooc/dto/User.java
+++ src/main/java/com/imooc/dto/User.java
@@ -1,13 +1,20 @@
 package com.imooc.dto;
 
+import com.fasterxml.jackson.annotation.JsonView;
+
 /**
  * create by mxy on 2017/12/18
  */
 public class User {
 
+    public interface UserSimpleView{};
+
+    public interface UserDetailView extends UserSimpleView{};
+
     private String name;
     private String passWord;
 
+    @JsonView(UserSimpleView.class)
     public String getName() {
         return name;
     }
@@ -16,6 +23,7 @@ public class User {
         this.name = name;
     }
 
+    @JsonView(UserDetailView.class)
     public String getPassWord() {
         return passWord;
     }
--- src/main/java/com/imooc/web/controller/UserController.java
+++ src/main/java/com/imooc/web/controller/UserController.java
@@ -1,10 +1,9 @@
 package com.imooc.web.controller;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import com.imooc.dto.User;
 import com.imooc.dto.UserQueryCondition;
-import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.bind.annotation.*;
-import sun.reflect.ReflectionFactory;
 
 import java.awt.print.Pageable;
 import java.util.ArrayList;
@@ -16,23 +15,22 @@ import java.util.List;
 
 
 @RestController
-
 public class UserController {
 
-    @RequestMapping(value = "/user", method = RequestMethod.GET)
-    // public List<User> query(@RequestParam(name = "nickName",required = false,defaultValue = "tom") String userName){
-    public List<User> query(UserQueryCondition condition, Pageable pageable) {
-
+    @GetMapping("/user")
+    @JsonView(User.UserSimpleView.class)
+    public List<User> query(@RequestParam(name = "nickName",required = false,defaultValue = "tom") String name){
+   // public List<User> query(UserQueryCondition condition, Pageable pageable) {
+        System.out.println(name);
         List<User> users = new ArrayList<>();
-        System.out.println(condition.toString());
-        System.out.println(pageable.toString());
         users.add(new User());
         users.add(new User());
         users.add(new User());
         return users;
     }
 
-    @RequestMapping(value = "/user/{id:\\d+}", method = RequestMethod.GET)
+    @GetMapping("/user/{id:\\d+}")
+    @JsonView(User.UserDetailView.class)
     public User getInfo(@PathVariable String id) {
         User user = new User();
         user.setName("tom");
